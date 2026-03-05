14498a45de18f5e72a4f61aad198692ebfef9a37
enzo <enzo.granana@ynov.com>
2026-03-05 12:49:27 +0100

    Add root route + changes server

--- server.ts
+++ server.ts
@@ -1,15 +1,23 @@
-import express from 'express';
+import express from "express";
+import path from "path";
 
 const server = express();
 const port = 3030;
 
-server.use(express.urlencoded({ extended: true }))
+server.use(express.urlencoded({ extended: true }));
 server.use(express.json());
+server.use(express.static(path.join(__dirname, "web/static")))
 
 server.set("view engine", "ejs");
+server.set("views", path.join(__dirname, "web/views"));
+server.set("routes", path.join(__dirname, "src/routes"));
+
+server.get("/", function (request, response) {
+    response.render("index")
+})
 
 server.listen(port, function () {
-    console.log("[Server]", "Server started on port", port)
-})
+    console.log("[Server]", "Server started on port", port);
+});
 
 export default server;
